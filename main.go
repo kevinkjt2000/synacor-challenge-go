27fff3147928a09ab799bbe837c6e8cb4a2d2b13
Kevin Tindall <kevinkjt2000@gmail.com>
2019-07-28 10:15:12 -0500

    Add all the uint16 instructions

--- main.go
+++ main.go
@@ -1,12 +1,33 @@
 package main
 
-type program map[int]byte
+type program map[int]uint16
 
 const (
-	halt byte = iota
+	halt uint16 = iota
+	set
+	push
+	pop
+	eq
+	gt
+	jmp
+	jt
+	jf
+	add
+	mult
+	mod
+	and
+	or
+	not
+	rmem
+	wmem
+	call
+	ret
+	out
+	in
+	noop
 )
 
-func runProgram(program map[int]byte) {
+func runProgram(program program) {
 	for pc := 0; ; pc++ {
 		instruction := program[pc]
 		if instruction == halt {
