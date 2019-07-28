a795601bf81b76aa897bde9edb517ba534f5703c
Kevin Tindall <kevinkjt2000@gmail.com>
2019-07-28 10:31:59 -0500

    Add default timeout to tests

--- main_test.go
+++ main_test.go
@@ -1,6 +1,18 @@
 package main
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
+
+func panicOnTimeout() {
+	<-time.After(500 * time.Millisecond)
+	panic("Test timed out")
+}
+
+func init() {
+	go panicOnTimeout()
+}
 
 func TestInstructionHalt(t *testing.T) {
 	program := program{
